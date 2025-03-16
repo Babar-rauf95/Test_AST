03e53dd3822d2a31c368f5ba709b7a8bea5abda7
usman95 <usmanrehan124@gmail.com>
2025-03-16 14:58:45 +0500

    Initial Commit

---
+++ FoodOrdering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodOrdering
+//
+//  Created by M Usman Bin Rehan on 16/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FoodOrderingTests/FoodOrderingTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderingTests.swift
+//  FoodOrderingTests
+//
+//  Created by M Usman Bin Rehan on 16/03/2025.
+//
+
+import Testing
+@testable import FoodOrdering
+
+struct FoodOrderingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
